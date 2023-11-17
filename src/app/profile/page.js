90ab11bbc1206b22582d3ed9d8468123b3c0b083
Farukul Islam <farukgb1999@gmail.com>
2023-11-17 23:18:12 +0600

    error solved

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,11 +1,29 @@
+"use client";
 import { selectUser } from "@/redux/slice/userSlice";
+import { Card } from "antd";
+import Image from "next/image";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const ProfilePage = () => {
   const user = useSelector(selectUser);
   console.log(user);
-  return <div>This is user page</div>;
+  return (
+    <div>
+      <Card
+        title="Card title"
+        bordered={false}
+        style={{
+          width: 300,
+        }}
+      >
+        <div>
+          <Image src="" alt="" />
+          <div></div>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default ProfilePage;
